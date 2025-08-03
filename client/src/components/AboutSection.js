a0ebe19544e220acb74e293e313f979f1ea2135e
tomansvk <tomanka.filip@gmail.com>
2025-08-03 16:49:52 +0200

    fixing mobile layout new 6

--- client/src/components/AboutSection.js
+++ client/src/components/AboutSection.js
@@ -19,7 +19,7 @@ export default function AboutSection() {
           </div>
           <div className="about-right">
             <img
-              src="/images/adam/adam1.jpg"
+              src="/images/adam/adam0.jpg"
               alt={t('AboutSection.header1')}
               className="about-image"
             />
